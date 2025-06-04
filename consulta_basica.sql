e4c2ef6d78547a9ecf4fa4ce2b3dcc194011a1a3
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-04 10:42:42 -0500

    feat: :bar_chart: se ha realizado asta el ejercicio 5

--- consulta_basica.sql
+++ consulta_basica.sql
@@ -13,5 +13,18 @@ WHERE tipo_id = 1  AND ciudad = 'Madrid';
 
 SELECT * FROM productos;
 
-SELECT nombre, precio FROM productos WHERE precio > 100.000;
+SELECT nombre, precio FROM productos WHERE precio > 100000;
 
+--4. Encuentra todos los empleados que tienen un salario superior a $2.500.000, mostrando su nombre, puesto y salario.
+
+SELECT * FROM empleados;
+
+SELECT usuarios.nombre, empleados.salario, empleados.puesto
+FROM usuarios
+JOIN empleados ON usuarios.usuario_id = empleados.usuario_id
+WHERE empleados.salario > 2500000;
+--5. Lista los nombres de los productos en la categoría "Electrónica", ordenados alfabéticamente.
+
+SELECT nombre , categoria FROM productos
+WHERE categoria = 'Electrónica' 
+ORDER BY categoria ASC;
