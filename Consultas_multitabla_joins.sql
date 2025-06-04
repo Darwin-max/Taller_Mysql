378b25c19cae9eb868d8816ca93fd1cccbdc6e6d
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-04 16:56:56 -0500

    feat: :bar_chart: Lista todos los productos pedidos junto con el precio unitario de cada pedido

--- Consultas_multitabla_joins.sql
+++ Consultas_multitabla_joins.sql
@@ -7,3 +7,11 @@ INNER JOIN detalles_pedidos AS dp ON p.pedido_id = dp.pedido_id
 WHERE u.tipo_id = 1;
 
 
+-- 2. Lista todos los productos pedidos junto con el precio unitario de cada pedido
+
+SELECT p.pedido_id, GROUP_CONCAT(CONCAT(po.nombre,  ',',po.categoria)) AS productos, SUM(dp.cantidad * dp.precio_unitario) as Total
+FROM pedidos AS p
+INNER JOIN detalles_pedidos AS dp ON p.pedido_id = dp.pedido_id
+INNER JOIN productos AS po ON dp.producto_id = po.producto_id
+GROUP BY p.pedido_id;
+
