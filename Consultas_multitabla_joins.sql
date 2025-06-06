28a177333cd7424a2ae52583f7462a9e52cc1cf8
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-05 20:24:06 -0500

    feat: Este es el ejercico 4, 5, 6 y  7

--- Consultas_multitabla_joins.sql
+++ Consultas_multitabla_joins.sql
@@ -20,8 +20,86 @@ GROUP BY p.pedido_id;
 SELECT 
   c.nombre AS nombre_cliente,
   e.nombre AS nombre_empleado
-FROM pedidos p
+FROM pedidos AS p
 INNER JOIN usuarios AS c ON p.cliente_id = c.usuario_id
 INNER JOIN empleados AS emp ON p.empleado_id = emp.empleado_id
 INNER JOIN usuarios AS e ON emp.usuario_id = e.usuario_id
 GROUP BY c.nombre, e.nombre;
+
+-- 4. Muestra todos los pedidos y, si existen, los productos en cada pedido, incluyendo los pedidos sin productos usando `LEFT JOIN`
+
+
+SELECT 
+    productos.nombre AS Producto,
+    pedidos.cliente_id AS Cliente_id,
+    cliente.nombre AS Cliente_Nombre,
+    pedidos.empleado_id AS Empleado_id,
+    empleado.nombre AS Empleado_Nombre,
+    detalles_pedidos.cantidad AS Cantidad,
+    pedidos.fecha_pedido AS Fecha,
+    pedidos.estado AS Estado
+FROM pedidos
+LEFT JOIN detalles_pedidos
+    ON pedidos.pedido_id = detalles_pedidos.pedido_id
+LEFT JOIN productos
+    ON detalles_pedidos.producto_id = productos.producto_id
+INNER JOIN usuarios AS cliente
+    ON pedidos.cliente_id = cliente.usuario_id
+INNER JOIN empleados
+    ON pedidos.empleado_id = empleados.empleado_id
+INNER JOIN usuarios AS empleado
+    ON empleados.usuario_id = empleado.usuario_id;
+
+-- 5. Encuentra los productos y, si existen, los detalles de pedidos en los que no se ha incluido el producto usando `RIGHT JOIN`.
+
+
+SELECT
+    productos.producto_id,
+    productos.nombre AS Nombre_producto,
+    productos.precio AS Precio_producto,
+    detalles_pedidos.cantidad AS Cantidad_pedido,
+    detalles_pedidos.detalle_id AS DetallePedido_id
+FROM detalles_pedidos
+RIGHT JOIN productos
+ON detalles_pedidos.producto_id = productos.producto_id;
+
+--6.  Lista todos los empleados junto con los pedidos que han gestionado, 
+-- si existen, usando `LEFT JOIN` para ver los empleados sin pedidos.
+
+SELECT 
+    empleados.empleado_id,
+    empleado.nombre AS Empleado_Nombre,
+    pedidos.pedido_id,
+    productos.nombre AS Producto_Nombre,
+    pedidos.fecha_pedido,
+    pedidos.estado
+FROM empleados
+LEFT JOIN pedidos
+    ON empleados.empleado_id = pedidos.empleado_id
+LEFT JOIN detalles_pedidos
+    ON pedidos.pedido_id = detalles_pedidos.pedido_id
+LEFT JOIN productos
+    ON detalles_pedidos.producto_id = productos.producto_id
+INNER JOIN usuarios AS empleado
+    ON empleados.usuario_id = empleado.usuario_id
+ORDER BY empleados.empleado_id;
+
+
+-- 7. Encuentra los empleados que no han gestionado ningún pedido usando un `LEFT JOIN` combinado con `WHERE`.
+
+SELECT    
+    empleados.empleado_id,
+    empleados.puesto AS Cargo,
+    empleados.fecha_contratacion AS FechaContratacion,
+    empleados.salario AS Salario
+FROM empleados
+LEFT JOIN pedidos
+    ON empleados.empleado_id = pedidos.empleado_id
+WHERE pedidos.pedido_id IS NULL;
+
+-- confirmacion que los empleados  tubieran pedidos, por si acaso XD
+
+SELECT empleados.empleado_id, pedidos.pedido_id
+FROM empleados
+RIGHT JOIN pedidos ON empleados.empleado_id = pedidos.empleado_id;
+
