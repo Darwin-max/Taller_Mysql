3a59443e034384ebe44e014cc3b609bc3afe77e8
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-05 22:03:01 -0500

    feat: :sparkles: subconsultas restantes 11 a la 23

--- Subconsultas.sql
+++ Subconsultas.sql
@@ -116,3 +116,165 @@ WHERE pedidos.fecha_pedido = (
     FROM pedidos
 );
 
+-- 11. Muestra los productos pedidos al menos una vez en los últimos 3 meses.
+
+SELECT productos.nombre AS Producto, productos.precio AS Precio
+FROM productos
+WHERE productos.producto_id IN (
+    SELECT detalles_pedidos.producto_id
+    FROM detalles_pedidos
+    JOIN pedidos ON detalles_pedidos.pedido_id = pedidos.pedido_id
+    WHERE pedidos.fecha_pedido >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
+);
+
+-- 12. Lista los empleados que no han gestionado ningún pedido.
+
+SELECT empleados.empleado_id AS EmpleadoID, empleados.puesto AS Puesto
+FROM empleados
+WHERE empleados.empleado_id NOT IN (
+    SELECT pedidos.empleado_id
+    FROM pedidos
+)
+
+-- 13. Encuentra los clientes que han 
+-- comprado más de tres tipos distintos de productos.
+
+SELECT usuarios.nombre AS Nombre, usuarios.ciudad AS Ciudad
+FROM usuarios
+WHERE usuarios.usuario_id IN (
+    SELECT pedidos.cliente_id
+    FROM pedidos
+    JOIN detalles_pedidos ON pedidos.pedido_id = detalles_pedidos.pedido_id
+    GROUP BY pedidos.cliente_id
+    HAVING COUNT(DISTINCT detalles_pedidos.producto_id) > 3
+);
+
+-- 14. Muestra el nombre del producto más caro que se ha pedido al menos cinco veces.
+
+SELECT productos.nombre, productos.precio
+FROM productos
+WHERE productos.producto_id IN (
+    SELECT producto_id
+    FROM detalles_pedidos
+    GROUP BY producto_id
+    HAVING COUNT(*) >= 5
+)
+ORDER BY productos.precio DESC
+LIMIT 1;
+
+
+-- 15. Lista los clientes cuyo primer pedido fue un año después de su registro.
+
+SELECT usuarios.*
+FROM usuarios
+WHERE(
+    SELECT 1
+    FROM pedidos
+    WHERE pedidos.cliente_id = usuarios.usuario_id
+    GROUP BY pedidos.cliente_id
+    HAVING MIN(pedidos.fecha_pedido) = DATE_ADD(usuarios.fecha_registro, INTERVAL 1 YEAR)
+);
+
+
+-- 16. Encuentra los nombres de los productos que tienen un stock inferior al promedio del stock de todos los productos.
+
+SELECT productos.nombre AS Productos, productos.stock AS Stock
+FROM productos
+WHERE productos.stock < (
+    SELECT AVG(stock)
+    FROM productos
+);
+
+-- 17. Lista los clientes que han realizado menos de tres pedidos.
+
+SELECT usuarios.nombre, usuarios.email, usuarios.pais
+FROM usuarios
+WHERE tipo_id = 1
+AND (
+    SELECT COUNT(*)
+    FROM pedidos
+    WHERE pedidos.cliente_id = usuarios.usuario_id
+) < 3;
+
+-- 18. Encuentra los nombres de los productos que fueron pedidos por los clientes que registraron en el último año.
+
+SELECT pedido_id, cliente_id, fecha_pedido
+FROM pedidos
+WHERE cliente_id IN (
+    SELECT usuario_id
+    FROM usuarios
+    WHERE fecha_registro >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+);
+
+-- 19. Obtén el nombre del empleado que gestionó el mayor número de pedidos.
+
+SELECT usuarios.nombre
+FROM empleados
+JOIN usuarios ON empleados.usuario_id = usuarios.usuario_id
+WHERE empleados.empleado_id = (
+    SELECT empleado_id
+    FROM pedidos
+    GROUP BY empleado_id
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
+);
+
+-- 20. Lista los productos que han sido comprados en cantidades mayores que el promedio de cantidad de compra de todos los productos.
+
+SELECT productos.nombre
+FROM productos
+WHERE producto_id IN (
+    SELECT producto_id
+    FROM detalles_pedidos
+    GROUP BY producto_id
+    HAVING SUM(cantidad) > (
+        SELECT AVG(total_cantidad)
+        FROM (
+            SELECT SUM(cantidad) AS total_cantidad
+            FROM detalles_pedidos
+            GROUP BY producto_id
+        ) AS subconsulta
+    )
+);
+
+-- 21. Proveedores que suministran más productos que el promedio de productos por proveedor.
+
+SELECT nombre
+FROM proveedores
+WHERE proveedor_id IN (
+    SELECT proveedor_id
+    FROM proveedores_productos
+    GROUP BY proveedor_id
+    HAVING COUNT(producto_id) > (
+        SELECT AVG(productos_por_proveedor)
+        FROM (
+            SELECT proveedor_id, COUNT(producto_id) AS productos_por_proveedor
+            FROM proveedores_productos
+            GROUP BY proveedor_id
+        ) AS subconsulta
+    )
+);
+
+-- 22. Proveedores que solo suministran productos de la categoría "Electrónica".
+SELECT *
+FROM proveedores
+WHERE proveedor_id IN (
+    SELECT proveedor_id
+    FROM proveedores_productos
+    WHERE producto_id IN (
+        SELECT producto_id
+        FROM productos
+        WHERE categoria = 'Electronica'
+    )
+);
+
+-- 23. Productos que solo tienen proveedores registrados hace más de un año.
+
+SELECT *
+FROM productos
+WHERE producto_id NOT IN (
+    SELECT producto_id
+    FROM proveedores_productos
+    JOIN proveedores USING(proveedor_id)
+    WHERE fecha_registro > DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+);
